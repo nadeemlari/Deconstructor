f028dc6c9c925ac18e1ef0a831d31a4fb4e0898a
Nadeem <nadeemlari@gmail.com>
2021-12-24 17:06:26 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+var nadeem = new Person
+{
+    FullName = "Nadeem Lari",
+    DateOfBirth = new DateOnly(2001, 6, 21)
+};
+var (fullName, dateOfBirth) = nadeem;
+
+Console.WriteLine($"The name is {fullName} , born in {dateOfBirth:MMM dd}");
+
+var (name, _) = nadeem;
+
+var dictionary = new Dictionary<string, int>
+{
+    {"Nadeem", 2001}
+};
+
+var (key, value) = dictionary.First();
+
+var dob = new DateOnly(2001, 6, 21);
+var (year, month, day) = dob;
+
+Console.WriteLine($"The name is {name}");
+public class Person
+{
+    public string FullName { get; init; } = "";
+    public DateOnly DateOfBirth { get; set; }
+
+    public void Deconstruct(out string fullName, out DateOnly dateOfBirth)
+    {
+        fullName = FullName;
+         dateOfBirth=DateOfBirth;
+    }
+}
+
+public static class DeconstructExtension
+{
+    public static void Deconstruct(this DateOnly dateOnly, out int year, out int month, out int day)
+    {
+        year = dateOnly.Year;
+        month = dateOnly.Month;
+        day = dateOnly.Day;
+    }
+}
